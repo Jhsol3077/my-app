a496a0861401b54eedc74a74bd3801b6fcf6e8cb
hansol <waw8805@hanmail.net>
2023-09-24 15:35:09 +0900

    카메라 권한 테스트

--- App.js
+++ App.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { WebView } from 'react-native-webview';
 import { StyleSheet, View, Text } from 'react-native';
 import Constants from 'expo-constants';
+import { scheduleNotificationAsync } from 'expo-notifications';
 
 export default function App() {
+  useEffect(() => {
+    // 앱이 시작될 때 알림 스케줄링
+    scheduleDailyNotification();
+  }, []);
+
+  async function scheduleDailyNotification() {
+    // 현재 시간과 매일 알림을 보낼 시간 설정 (24시간 형식)
+    const trigger = {
+      hour: 15,
+      minute: 2,
+      repeats: true, // 매일 반복
+    };
+
+    // 알림 내용 및 스케줄링
+    await scheduleNotificationAsync({
+      content: {
+        title: '매일 알림',
+        body: '알림 내용을 입력하세요.',
+      },
+      trigger,
+    });
+  }
+
   return (
     <View style={styles.container}>
       <WebView
